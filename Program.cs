28b69dc1cf7f694965d03fef1d730ee34f8582e0
Jack Lindgren <jackmarkcharleslindgren@gmail.com>
2025-10-14 12:25:52 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_App_For_Azure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string loopSentinal = "y";
+            string response;
+            do
+            {
+                Console.Write("Do you want to close this program? (Y/N): ");
+                response = Console.ReadLine().Trim().Substring(0, 1).ToLower();
+            } 
+            while (response != loopSentinal);
+            Console.Write("Press any key to exit ");
+            Console.ReadKey();
+        }
+    }
+}
