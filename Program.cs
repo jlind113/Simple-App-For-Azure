75183100f6f024cdd1deb6e57afc9f2003d9734f
Jack Lindgren <jackmarkcharleslindgren@gmail.com>
2025-10-16 13:56:23 -0600

    Add testable functions

--- Program.cs
+++ Program.cs
@@ -15,12 +15,73 @@ namespace Simple_App_For_Azure
             string response;
             do
             {
+                Console.Write("Enter the first number: ");
+                double numberOne = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter the second number: ");
+                double numberTwo = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Choose your function");
+                Console.Write("(Addition/A), (Subtraction/S), (Multiplication/M), (Division/D): ");
+                string function = Console.ReadLine().Trim().Substring(0, 1).ToLower();
+
+                switch (function)
+                {
+                    case "a":
+                        double result = Addition(numberOne, numberTwo);
+                        Console.WriteLine(numberOne.ToString() + " + " + numberTwo.ToString() + " = " + result.ToString());
+                        break;
+                    case "s":
+                        result = Subtraction(numberOne, numberTwo);
+                        Console.WriteLine(numberOne.ToString() + " - " + numberTwo.ToString() + " = " + result.ToString());
+                        break;
+                    case "m":
+                        result = Multiplication(numberOne, numberTwo);
+                        Console.WriteLine(numberOne.ToString() + " * " + numberTwo.ToString() + " = " + result.ToString());
+                        break;
+                    case "d":
+                        result = Division(numberOne, numberTwo);
+                        Console.WriteLine(numberOne.ToString() + " / " + numberTwo.ToString() + " = " + result.ToString());
+                        break;
+                    default:
+                        Console.WriteLine(function + " is not a valid function");
+                        break;
+                }
+
                 Console.Write("Do you want to close this program? (Y/N): ");
                 response = Console.ReadLine().Trim().Substring(0, 1).ToLower();
             } 
             while (response != loopSentinal);
             Console.Write("Press any key to exit ");
             Console.ReadKey();
+        }
+        static double Addition(double numberOne, double numberTwo)
+        {
+            double result = numberOne + numberTwo;
+            return result;
+        }
+        static double Subtraction(double numberOne, double numberTwo)
+        {
+            double result = numberOne - numberTwo;
+            return result;
+        }
+        static double Multiplication(double numberOne, double numberTwo)
+        {
+            double result = numberOne * numberTwo;
+            return result;
+        }
+        static double Division(double numberOne, double numberTwo)
+        {
+            if (numberOne != 0 && numberTwo != 0)
+            {
+                double result = numberOne / numberTwo;
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero");
+                return 0;
+            }
         }
     }
 }
